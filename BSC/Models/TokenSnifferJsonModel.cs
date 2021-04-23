e27f6ce7dbadedaa69b458ba6bb15b30e13e62b8
Mo <mo@it0.me>
2021-04-23 09:59:30 +0200

    Add project files.

---
+++ BSC/Models/TokenSnifferJsonModel.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSC.Models {
+    public static class TokenSnifferJsonModel {
+        // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
+        public class EthToken {
+            public string addr { get; set; }
+            public string name { get; set; }
+            public string symbol { get; set; }
+            public bool defunct { get; set; }
+            public string image_url { get; set; }
+            public string homepage_url { get; set; }
+            public string forum_url { get; set; }
+            public string chat_url { get; set; }
+            public string announcement_url { get; set; }
+            public string code_url { get; set; }
+            public string twitter_name { get; set; }
+            public DateTime created_at { get; set; }
+            public object network { get; set; }
+            public bool has_source_code { get; set; }
+            public bool autoflag { get; set; }
+            public string source_md5 { get; set; }
+            public object exploit_id { get; set; }
+        }
+
+        public class BscToken {
+            public string addr { get; set; }
+            public string name { get; set; }
+            public string symbol { get; set; }
+            public bool defunct { get; set; }
+            public string image_url { get; set; }
+            public string homepage_url { get; set; }
+            public string forum_url { get; set; }
+            public string chat_url { get; set; }
+            public string announcement_url { get; set; }
+            public string code_url { get; set; }
+            public string twitter_name { get; set; }
+            public DateTime created_at { get; set; }
+            public string network { get; set; }
+            public bool has_source_code { get; set; }
+            public bool autoflag { get; set; }
+            public string source_md5 { get; set; }
+            public object exploit_id { get; set; }
+        }
+
+        public class PageProps {
+            public List<EthToken> ethTokens { get; set; }
+            public List<BscToken> bscTokens { get; set; }
+        }
+
+        public class Props {
+            public PageProps pageProps { get; set; }
+            public bool __N_SSG { get; set; }
+        }
+
+        public class Query {
+        }
+
+        public class Root {
+            public Props props { get; set; }
+            public string page { get; set; }
+            public Query query { get; set; }
+            public string buildId { get; set; }
+            public bool nextExport { get; set; }
+            public bool isFallback { get; set; }
+            public bool gsp { get; set; }
+            public List<List<object>> head { get; set; }
+        }
+
+
+    }
+}
---
+++ BSC/Program.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace BSC {
+    class Program {
+        const string TokenSnifferUrl = "https://tokensniffer.com/tokens/new";
+        static void Main(string[] args) {
+            using var webClient = new WebClient();
+
+            var tokenSnifferSource = webClient.DownloadString(TokenSnifferUrl);
+
+            var rawJsonData = GetJsonFromSource(tokenSnifferSource);
+            var jsonData = JsonConvert.DeserializeObject<Models.TokenSnifferJsonModel.Root>(rawJsonData);
+
+            var bscTokens = jsonData.props.pageProps.bscTokens;
+        }
+
+        private static string GetJsonFromSource(string tokenSnifferSource) {
+            var regex = new System.Text.RegularExpressions.Regex(@"\<script\s?.*?\>((.|\r\n)+?)\<\/script\>");
+            var v = regex.Match(tokenSnifferSource);
+            return v.Value.Replace(@"<script id=""__NEXT_DATA__"" type=""application/json"">", "").Replace("</script>", "");
+
+            //string output = System.Text.RegularExpressions.Regex.Replace(input, "<Video>(.*)</Video>", string.Format("<Video>www.test.com</Video>"))
+        }
+    }
+}
